d899b10d5d641cb193bc2bb73faae9881eea1dd4
larg5847 <spartan5847@hotmail.com>
2020-01-31 23:55:49 -0600

    gui de control y movimiento de personaje principal

---
+++ UTSKIN/Assets/scripts/cPersonaje.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cPersonaje : MonoBehaviour
+{
+
+    Vector3 direccion;
+    int cantBalas = 20;
+
+    public Joystick mov;
+    public Joystick dir;
+    
+
+    int velocidad = 5;
+    float rot=0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        direccion = this.GetComponent<Transform>().position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movimientoJugador();
+        rotacionJugador();
+        disparando();
+    }
+
+    private void movimientoJugador()
+    {
+        direccion = new Vector3(mov.Horizontal, mov.Vertical);
+        direccion *= velocidad * Time.deltaTime;
+        this.transform.position += direccion;
+        
+    }
+
+    private void rotacionJugador()
+    {
+        float z = dir.Horizontal * 300;   
+        rot -= Mathf.Round(z) * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(0, 0, rot);
+    }
+
+    public void disparando()
+    {
+
+        cantBalas -= 1;
+    }
+}
---
+++ UTSKIN/Assets/scripts/followCam.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followCam : MonoBehaviour
+{
+
+    GameObject jugador;
+    Vector3 separacionDeMovimiento;
+    // Start is called before the first frame update
+    void Start()
+    {
+        jugador = GameObject.FindGameObjectWithTag("Player");
+        Debug.Log(jugador.name);
+        separacionDeMovimiento = this.transform.position - jugador.transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        this.transform.position = jugador.transform.position + separacionDeMovimiento;
+    }
+}
